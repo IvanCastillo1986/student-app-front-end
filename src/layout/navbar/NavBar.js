80859591a11271bc41c9df5b942bb0429d82c7e2
Ivan Castillo <total180training@gmail.com>
2022-08-09 17:14:17 -0400

    overlay and scroll lock on mobile navbar

--- src/layout/navbar/NavBar.js
+++ src/layout/navbar/NavBar.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
 import './NavBar.scss'
@@ -22,12 +23,40 @@ const menuItems = [
 
 export default function NavBar(props) {
 
+    let navigate = useNavigate()
     const [showMenu, setShowMenu] = useState(false)
+    const [showNavbarItems, setShowNavbarItems] = useState(false)
+
+    useEffect(() => {
+        if (showNavbarItems) {
+            document.body.style.overflow = "hidden"
+        } else {
+            document.body.style.overflow = "auto"
+        }
+    }, [showNavbarItems])
+
+    // CURRENT ERROR: NEED TO MAKE TOGGLEMENUITEMS HANDLE FOR SETSHOWNAVBARITEMS
+    const handleNavigation = (e) => {
+        // close menu
+        setShowMenu(!showMenu)
+        setShowNavbarItems(false)
+        // navigate to new page
+        navigate(e.target.dataset.url)
+    }
+
 
     return (
         <div className='navbar'>
+            <div 
+                className="navbar__overlay" 
+                style={{display: showNavbarItems ? "block" : "none"}}
+                onClick={() => {
+                    setShowNavbarItems(false);
+                    setShowMenu(false)
+                }}
+            ></div>
             <div className="navbar__links">
-                <div className='navbar__logo'>
+                <div className='navbar__logo' onClick={() => setShowNavbarItems(false)}>
                     <Link to='/'>Student App</Link>
                 </div>
 
@@ -35,14 +64,21 @@ export default function NavBar(props) {
                     <ul>
                         {menuItems.map((menuItem, key) => {
                             return(
-                            <li key={key}>
-                                <Link to={menuItem.url}>{menuItem.label}</Link>
+                            <li key={key} onClick={() => handleNavigation} data-url={menuItem.url}>
+                                {/* <Link to={menuItem.url} onClick={() => setShowMenu(!showMenu)}>{menuItem.label}</Link> */}
+                                {menuItem.label}
                             </li>
                             )
                         })}
                     </ul>
                 </div>
-                <div className="navbar__hamburger" onClick={() => {setShowMenu(!showMenu)}}>=</div>
+                <div 
+                    className="navbar__hamburger" 
+                    onClick={() => {
+                        setShowMenu(!showMenu); 
+                        setShowNavbarItems(!showNavbarItems);
+                    }
+                }>=</div>
             </div>
         </div>
     )
